396d9ddfd43d9b64ede65d361456b65d0a722c60
Mohammad <mohammad.sabbagh@gmail.com>
2022-09-04 01:20:41 +0300

    Update

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -9,8 +9,8 @@ export default function Contacts() {
 
 
           <div className="column has-text-centered">
-            <p><a href="https://wa.me/971526234959">WhatsApp</a></p>
-            <p><a href="tel:+971526234959">+971 52 623 4959</a></p>
+            <p><a href="https://wa.me/971528811474">WhatsApp</a></p>
+            <p><a href="tel:+971528811474">+971 52 88 11 474</a></p>
             <p><a href="mailto:nasserrustom@gmail.com">nasserrustom@gmail.com</a></p>
             <br/>
             <p>DeepBlueFishing</p>
